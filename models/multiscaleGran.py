b5f0aa298472188d25f41b4d41e5ec52ad82f70d
albertxu <17albertX@students.harker.org>
2018-09-01 17:17:49 -0400

    sped up calculations to 0.2 s per cycle

--- models/multiscaleGran.py
+++ models/multiscaleGran.py
@@ -1,5 +1,5 @@
 from scipy.spatial import distance
-from peopleget.peopledata import PersonData, PeopleData, getdeopledata
+from models.peopleget.peopledata import PersonData, PeopleData, getdeopledata
 from math import pow, cos, atan2, sin
 import numpy as np
 import timeit
@@ -41,39 +41,33 @@ class SimpleRules(object):
         print("Phi: ", self.phi)
         print("D max: ", self.d_max)
 
-    def spaceToHit(self, space, angle, actpos):
-        hitpoints = []
-        for x in range(space.shape[0]):
-            for y in range(space.shape[1]):
-                if space[x, y]:
-                    newx = x - actpos[0]
-                    newy = y - actpos[1]
-                    newx = newx*cos(angle) - newy*sin(angle)
-                    newy = newx*sin(angle) + newy*cos(angle)
-                    hitpoints.append((newx, newy))
-        return hitpoints
+    # def spaceToHit(self, space, angle, actpos):
+    #     coords = np.array(np.where(space)).T - actpos
+    #
+    #     rot_mat = np.array([[cos(angle), -sin(angle)], [sin(angle), cos(angle)]])
+    #     hitpoints = np.matmul(rot_mat, coords.T).T
+    #
+    #     return hitpoints
 
     def Fa(self, angles, pos, obsSpace):
         fas = []
-        for angle in angles:
-            obst = self.spaceToHit(obsSpace, angle, pos)
-            hits = [obs for obs in obst if abs(obs[1]) < self.r_i]
-            neardist = hits[0][0]
-            for obs in hits:
-                if 0 < obs[0] < neardist:
-                    neardist = obs[0]
+        coords = (np.array(np.where(obsSpace)).T - pos).T
+        c, s = np.cos(angles), np.sin(angles)
+        rots = np.array([[c, -s], [s, c]]).transpose((2, 0, 1))
+        for rot_mat in rots:
+            # rot_mat = np.array([[cos(angle), -sin(angle)], [sin(angle), cos(angle)]])
+            # obst = self.spaceToHit(obsSpace, angle, pos)
+            obst = np.matmul(rot_mat, coords)
+            hits = obst[0, ((np.abs(obst[1]) < self.r_i) & (obst[0] > 0))]
+
+            neardist = np.amin(hits)
             fas.append(neardist)
 
-        return angles
-
-    def Vdes(self, v0, dh):
-        if v0 > dh/self.t:
-            return dh/self.t
-        else:
-            return v0
+        return np.array(fas)
 
     def Da(self, alpha, alpha_0, fa):
-        return [pow(self.d_max, 2) + pow(fa_i, 2) - 2*self.d_max*fa_i*cos(alpha_0-alph) for alph, fa_i in zip(alpha, fa)]
+        return self.d_max**2 + fa**2 - 2*self.d_max*fa*np.cos(alpha_0 - alpha)
+        # return [self.d_max**2 + fa_i**2 - 2*self.d_max*fa_i*cos(alpha_0-alph) for alph, fa_i in zip(alpha, fa)]
 
     # If the disappear, their destination is where they disappeared.
     # If they walked off an edge, their dest is closest to edge
@@ -110,9 +104,12 @@ class SimpleRules(object):
             return dest
 
     def propagate(self, startstep, peopledata, nsteps):
-        people = [(p[1], p[2]) for p in peopledata.stateS(startstep)]
-        peopleN =[(p[1], p[2]) for p in peopledata.stateS(startstep + 1)]
-        orientations = [atan2(pn[1]-p[1], pn[0]-p[0]) for p, pn in zip(people, peopleN)]
+        people = np.array([(p[1], p[2]) for p in peopledata.stateS(startstep)])
+        peopleN =np.array([(p[1], p[2]) for p in peopledata.stateS(startstep + 1)])
+
+        ds = peopleN - people
+
+        orientations = np.arctan2(ds[:,1], ds[:,0])
         termBoundries = [self.getTerm(p, self.obstSpace.shape) for p in peopledata.people]
         '''
         boundaries are defined as such
@@ -127,31 +124,46 @@ class SimpleRules(object):
      (0,0)       MinY
         '''
         for n in range(nsteps):
-
             tStamp = timeit.default_timer()
             dests = [self.getDest(term, self.obstSpace.shape) for term in zip(people, termBoundries)]
             desorientations = [atan2(dest[1]-pos[1], dest[0]-dest[0]) for pos, dest in zip(people, dests)]
-            fullObsSpace = np.full((640, 480), False, dtype=bool)
 
+            fullObsSpace = np.full((640, 480), False, dtype=bool)
+            yc, xc = np.ogrid[:640, :480]
             for p in people:
-                for i in range(int(max(p[0]-self.r_i, 0)), int(min(p[0]+self.r_i, self.obstSpace.shape[0]-1))):
-                    for j in range(int(max(p[1]-self.r_i, 0)), int(min(p[1]+self.r_i, self.obstSpace.shape[0]-1))):
-                        if distance.euclidean(p[0], (i, j)) < self.r_i:
-                            fullObsSpace[i, j] = True
+                distsq = (xc - p[0])**2 + (yc - p[1])**2
+                circ_mask = distsq <= self.r_i**2
+
+                fullObsSpace[circ_mask] = True
+            print('generating obstacle map from people\t: {} s'.format(timeit.default_timer()-tStamp))
+
             fullObsSpace = np.logical_or(fullObsSpace, self.obstSpace)
 
             alpha_des = []
             d_h = []
+            tsi = timeit.default_timer()
             for p, an, dAn in zip(people, orientations, desorientations):
                 angles = np.linspace(an-self.phi, an+self.phi, self.angcheck)
                 fa = self.Fa(angles, p, fullObsSpace)
                 da = self.Da(angles, dAn, fa)
-                alpha_des.append(angles[da.index(min(da))])
-                d_h.append(fa[da.index(min(da))])
-            v_des = [min(self.vi0, dh/self.t) for dh in d_h]
-            people = [(p[0]+cos(alpha)*v*self.t, p[1]+sin(alpha)*v*self.t) for p, alpha, v in zip(people, alpha_des,v_des)]
+
+                min_da = np.argmin(da)
+
+                alpha_des.append(da[min_da])
+                d_h.append(fa[min_da])
+            print('main loop\t: {} s'.format(timeit.default_timer()-tsi))
+            alpha_des = np.array(alpha_des)
+            d_h = np.array(d_h)
+
+            tsi = timeit.default_timer()
+            v_des = np.minimum(d_h/self.dt, self.vi0)
+            v_des_v = v_des * np.array([np.cos(alpha_des), np.sin(alpha_des)])
+
+            people = people + v_des_v.T * self.dt
             orientations = alpha_des
-            print("One Step took: {} Minutes".format((timeit.default_timer()-tStamp)/60.0))
+            self.t += self.dt
+            print('the rest: {} s'.format(timeit.default_timer() - tsi))
+            print("One Step took: {} s".format((timeit.default_timer()-tStamp)))
         return [(p[0], p[1], v*cos(o), v*sin(o)) for p, v, o in zip(people, v_des, orientations)]
 
 if __name__ == "__main__":
